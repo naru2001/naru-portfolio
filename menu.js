5640ec102780c9939ad5000598a4b6c8a9921496
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-08-25 19:45:33 +0900

    add hobbys

--- menu.js
+++ menu.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btn = document.querySelector('.menu-btn');
     const nav = document.querySelector('nav');
+    const m = document.querySelector("main");
     btn.addEventListener('click', function () {
-        nav.classList.toggle('open-menu')
+        nav.classList.toggle('open-menu');
+        m.classList.toggle('open-menu');
         if (btn.innerHTML === 'メニュー') {
             btn.innerHTML = '閉じる';
         } else {
