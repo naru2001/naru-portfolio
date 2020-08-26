488b99ffc0812a390e01768acff04fdf8b43945c
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-08-26 20:26:31 +0900

    a

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const btn = document.querySelector('.menu-btn');
     const nav = document.querySelector('nav');
     const m = document.querySelector("main");
+    const tab = document.querySelector(".tabmenu");
     btn.addEventListener('click', function () {
-        nav.classList.toggle('open-menu');
-        m.classList.toggle('open-menu');
+        var z="open-menu";
+        nav.classList.toggle(z);
+        m.classList.toggle(z);
+        tab.classList.toggle(z);
         if (btn.innerHTML === 'メニュー') {
             btn.innerHTML = '閉じる';
         } else {
